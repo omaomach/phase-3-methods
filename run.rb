8551d6da7c945a32376b03739773e5419e272839
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-16 14:15:03 +0300

    completed phase 3 methods

---
+++ run.rb
@@ -0,0 +1,48 @@
+def my_function(param)
+    puts "run ruby run"
+    return param + 1
+end
+# puts my_function(1)
+
+def engineer
+    "Software Engineer Joash Omao"
+end
+
+# puts engineer
+
+def say_hello(name = "Joshua")
+    puts "Hi there, #{name}"
+end
+# say_hello("Bongoye")
+
+def multiply_and_log(num1, num2)
+    puts num1 * num2
+end
+
+def divide_and_return(num1, num2)
+    return num1 / num2
+end
+
+# result1 = multiply_and_log(4,3)
+# result2 = divide_and_return(21, 3)
+# puts result1.class # returns a nil
+# puts result2.class # returns an integer
+
+def print_name
+    puts "Faith Mwabe"
+end
+
+def return_name
+    "Gloria Nyatichi"
+end
+
+# print_name
+# puts return_name
+
+def software_engineer
+    best_engineer = "Joash"
+    return "Omao"
+    best_engineer
+end
+
+puts software_engineer
